c3cd687a777574da9628201dfca4fbfce5bfe145
Eben Lowe <eben.lowe@gmail.com>
2019-04-10 15:38:10 -0400

    Initial commit.

---
+++ app/javascript/react/AppRouter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hello from './Hello'
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+const AppRouter = props => (
+  <Router>
+    <div>
+      <Route path="/" exact component={Hello} />
+    </div>
+  </Router>
+)
+
+export default AppRouter
---
+++ app/javascript/react/Hello.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Hello = () => <h1>Boo yaa</h1>
+
+export default Hello
